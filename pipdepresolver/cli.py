62ea16b175243d028e68407809c0116600a7aaf7
Vincent Philippon <sindaewoh@gmail.com>
2017-10-27 00:36:04 -0400

    Minimal cleanup
    This looks like we might get to something...

--- pipdepresolver/cli.py
+++ pipdepresolver/cli.py
@@ -17,17 +17,16 @@ def cli():
 @click.pass_context
 def install(ctx, src_file):
 
-    # # 1. Run a first `pip install -r src_file`.
+    # 1. Run a first `pip install -r src_file`.
     try:
         output = subprocess.check_output(['pip', 'install', '-r', src_file])
-        print(output)
+        click.echo(output)
     except subprocess.CalledProcessError as exc:
-        print(exc.message)
+        click.echo(exc.message, err=True)
         ctx.exit(exc.returncode)
 
-
     # 2. Get in a cycle of `pip check` & `pip install <reqs>`.
-    for _ in range(10):
+    for _ in range(10):  # Max 10 rounds.
         # 2.1. `pip check` to get incompatible dependencies.
         try:
             output = subprocess.check_output(['pip', 'check'])
@@ -36,19 +35,17 @@ def install(ctx, src_file):
             if exc.returncode == 1 and exc.message == '':
                 output = exc.output
             else:
-                print(exc.message)
-                print('!!!!!!!!!!!!!!!!!')
+                click.echo(exc.message, err=True)
                 ctx.exit(exc.returncode)
 
-        # 2.2 `pip install <missing dependencies>
+        # 2.2 `pip install <missing dependencies>` to fix.
         reqs = re.findall(CHECK_RE, output, flags=re.MULTILINE)
-        print(reqs)
         if reqs:
             try:
                 output = subprocess.check_output(['pip', 'install', ' '.join(reqs)])
-                print(output)
+                click.echo(output)
             except subprocess.CalledProcessError as exc:
-                print(exc.message)
+                click.echo(exc.message, err=True)
                 ctx.exit(exc.returncode)
         else:
             # 2.3 All dependencies compatible
@@ -56,4 +53,4 @@ def install(ctx, src_file):
             break
     else:
         # 2.4 Cycled indefinitely without finding a solution.
-        print('OH NO!')
+        print('OH NO! No solution found. Check your dependencies.')
